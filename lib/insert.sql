acb5d4fde3395b4cd634f4a181c31353d5d8dd34
Shilcof <robert.shilcof@gmail.com>
2020-11-23 14:44:04 +0000

    commit tree 83fa6dc40606e1a982effb2e23666e8a1a0b1030

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 10, "M", "Brown", "Friendly", 1),
+("Rowdy", 10, "M", "Black", "Friendly", 1),
+("Tabitha", 10, "F", "Brown", "Friendly", 1),
+("Sergeant Brown", 10, "M", "Brown", "Friendly", 1),
+("Melissa", 10, "F", "Black", "Friendly", 1),
+("Grinch", 10, "M", "Brown", "Friendly", 1),
+("Wendy", 10, "F", "Brown", "Friendly", 1),
+(NULL , 10, "M", "Black", "Unfriendly", 1);
